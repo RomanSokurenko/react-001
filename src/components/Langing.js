f70666c68e6bb8aaf86114b5ff296bf97537d318
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-07-28 23:52:31 +0300

    save

---
+++ src/components/Langing.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import restaurants from '../sample-restaurants';
+
+
+
+
+class Langing extends React.Component {
+   render() {
+      return <div className='restaurant_select'>
+         <div className='restaurant_select_top'>
+            <div className='restaurant_select_top_header'>
+               Выбери ресторан
+            </div>
+            <div className='arrow_picker'>
+            </div>
+            <div className='arrow_picker-up'>
+            </div>
+            <div className='arrow_picker-down'>
+            </div>
+            <div className='restaurant_select-button'>
+               <ul>
+                  {
+                     restaurants.map(restaurant => {
+                        return <li key={restaurant.id}> {restaurant.title}</li>
+                     })
+                  }
+               </ul>
+            </div>
+         </div>
+         <button>
+            Перейти в ресторан
+         </button>
+      </div>
+
+
+
+
+   }
+}
+
+export default Langing;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hello');
+import React from 'react';
+import { render } from 'react-dom';
+import Langing from './components/Langing'
+
+render(<Langing />, document.querySelector('#root'));
